02ce0ae8c224bdae6a2b60788e2a066c51a7c8aa
charlieproodle <charlie@proodlesolutions.com>
2019-07-11 15:15:02 +0100

    Update CIFAR10/models.py

--- CIFAR10/models.py
+++ CIFAR10/models.py
@@ -5,31 +5,6 @@ from keras.optimizers import Adam
 import imageio
 import glob
 
-filenames = glob.glob("MNIST/images/*.png")
-images = []
-for file in filenames:
-    images.append(imageio.imread(file))
-
-imageio.mimsave("MNIST/mnist.gif", images, fps = 2)
-
-#
-# with imageio.get_writer("cifar10.gif", mode = "I") as writer:
-#     filenames = glob.glob("CIFAR10/images/*.png")
-#     filenames = sorted(filenames)
-#     last = -1
-#     for i,filename in enumerate(filenames):
-#         frame = 100*(i**0.5)
-#         if round(frame) > round(last):
-#             last = frame
-#         else:
-#             continue
-#         image = imageio.imread(filename)
-#         writer.append_data(image)
-#     image = imageio.imread(filename)
-#     writer.append_data(image)
-
-
-
 class Models():
 
     def generator(self, latent_dim):
